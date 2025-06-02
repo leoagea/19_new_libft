e37b0bf96ddbae0280eb168fdd345a1acace37db
leoagea <leoagea@gmail.com>
2025-06-02 19:28:47 +0200

    fix: enhance dll_quick_sort , removing leaks

--- src/double_linked_list/dll_quick_sort.c
+++ src/double_linked_list/dll_quick_sort.c
@@ -6,7 +6,7 @@
 /*   By: lagea < lagea@student.s19.be >             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 18:11:54 by lagea             #+#    #+#             */
-/*   Updated: 2025/06/02 18:28:49 by lagea            ###   ########.fr       */
+/*   Updated: 2025/06/02 19:24:03 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,40 +14,66 @@
 
 void dll_quick_sort(t_dll *list, int (*cmp)(void *, void *))
 {
-	if (!list || !list->head || !list->tail || list->head == list->tail)
-		return;
+    if (!list || !list->head || !list->tail || list->head == list->tail)
+        return;
 
-	t_node *pivot = list->head;
-	t_dll left;
-	dll_init(&left);
-	// t_node *left = NULL;
-	t_dll right;
-	dll_init(&right);
-	// t_node *right = NULL;
-	t_node *current = list->head->next;
+    t_node *pivot = list->head;
+    t_dll left;
+    t_dll right;
+    dll_init(&left);
+    dll_init(&right);
 
-	while (current) {
-		if (cmp(current->content, pivot->content) < 0) {
-			dll_insert_tail(current->content, &left);
-		} else {
-			dll_insert_tail(current->content, &right);
-		}
-		current = current->next;
-	}
+    t_node *current = list->head->next;
+    t_node *next;
 
-	dll_quick_sort(&left, cmp);
-	dll_quick_sort(&right, cmp);
+    while (current) {
+        next = current->next;
+        if (cmp(current->content, pivot->content) < 0) {
+            current->prev->next = current->next;
+            if (current->next)
+                current->next->prev = current->prev;
+            current->next = NULL;
+            current->prev = left.tail;
+            if (!left.head)
+                left.head = current;
+            if (left.tail)
+                left.tail->next = current;
+            left.tail = current;
+            left.size++;
+        } else {
+            current->prev->next = current->next;
+            if (current->next)
+                current->next->prev = current->prev;
+            current->next = NULL;
+            current->prev = right.tail;
+            if (!right.head)
+                right.head = current;
+            if (right.tail)
+                right.tail->next = current;
+            right.tail = current;
+            right.size++;
+        }
+        current = next;
+    }
 
-	list->head = left.size != 0 ? left.head : pivot;
-	if (left.size > 0) {
-		left.tail->next = pivot;
-		pivot->prev = left.tail;
-	}
-	pivot->next = right.size != 0 ? right.head : NULL;
-	if (right.size > 0) {
-		right.head->prev = pivot;
-		list->tail = right.tail;
-	} else {
-		list->tail = pivot;
-	}
+    // Recursively sort sublists
+    dll_quick_sort(&left, cmp);
+    dll_quick_sort(&right, cmp);
+
+    // Reconstruct the main list
+    list->head = left.head ? left.head : pivot;
+    if (left.tail) {
+        left.tail->next = pivot;
+        pivot->prev = left.tail;
+    } else {
+        pivot->prev = NULL;
+    }
+
+    pivot->next = right.head;
+    if (right.head) {
+        right.head->prev = pivot;
+        list->tail = right.tail;
+    } else {
+        list->tail = pivot;
+    }
 }
